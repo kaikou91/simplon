44548df178234fc60ded01e831a459d0b7d1e6a9
seifeddine <ben.kaikou@gmail.com>
2021-02-02 10:38:39 +0100

    added selection colors

--- exo-signup/main.js
+++ exo-signup/main.js
@@ -1,8 +1,21 @@
 const alreadyRegisteredUsernames = ["kim", "emmanuel", "nicolas"];
 
 document.querySelector("form").addEventListener("submit", () => {
-  // avant d'envoyer les données au serveur, on peut faire une étape de validation
+    // avant d'envoyer les données au serveur, on peut faire une étape de validation
 
-  // on n'envoie pas vraiment les données pour l'exercice
-  document.querySelector("main").innerHTML = "Compte créé :)";
+    // on n'envoie pas vraiment les données pour l'exercice
+    document.querySelector("main").innerHTML = "Compte créé :)";
 });
+
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+let mdp = document.getElementById('password');
+let mdp_conf = document.getElementById('comfirm-password');
+
+// if (mdp.value == "") {
+//     alert("Saisissez votre mot de passe");
+//     password.focus();
+//     return false;
+// }
